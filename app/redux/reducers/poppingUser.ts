4d617660fcf29f9e568d8ca0f0e8b6fc170ac9ec
HarenKei <seungmin1266@gmail.com>
2024-08-19 13:53:25 +0900

    Refactor: Change type interface of popping users

--- app/redux/reducers/poppingUser.ts
+++ app/redux/reducers/poppingUser.ts
@@ -6,27 +6,11 @@ const initialState = {
     nickname: '',
     name: '',
     isMale: null,
-    businessInfo: {
-      businessNumber: '',
-      startDate: '',
-      participantName: '',
-      //일반 user -> 빈 객체 반환
-    },
     phoneNumber: '',
     uuid: '',
-    createdAt: '',
     isPopper: false,
     isSocialuser: false,
     socialLoginProvider: '',
-    gradeInfo: {
-      grade: '',
-      minOrderAmount: 0,
-      maxOrderAmount: 0,
-      earnRate: 0,
-      discountRate: 0,
-    },
-    point: 0,
-    savedPopup: [],
   }
 
 };
