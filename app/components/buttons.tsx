234f1bcf44345f5a68531f9c026468a5ca485925
HarenKei <seungmin1266@gmail.com>
2024-08-20 00:57:39 +0900

    Feat: Add Large single button Components

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -5,17 +5,109 @@ type ButtonTypes = {
   backgroundColor: string;
   textColor: string;
   onClick: () => void;
+  border?: string;
 };
 
 type ButtonStyleTypes = {
   backgroundColor: string;
   textColor: string;
+  border?: string;
 };
+
+export const ButtonLargeSingle = ({
+  text,
+  backgroundColor,
+  textColor,
+  onClick,
+  border,
+}: ButtonTypes) => {
+  return (
+    <LargeButtonSingle
+      backgroundColor={backgroundColor}
+      textColor={textColor}
+      onClick={onClick}
+      border={border}
+    >
+      <p>{text}</p>
+    </LargeButtonSingle>
+  );
+};
+
+export const ButtonLarge = ({
+  text,
+  backgroundColor,
+  textColor,
+  onClick,
+  border,
+}: ButtonTypes) => {
+  return (
+    <LargeButton
+      backgroundColor={backgroundColor}
+      textColor={textColor}
+      onClick={onClick}
+      border={border}
+    >
+      <p>{text}</p>
+    </LargeButton>
+  );
+};
+
+export const ButtonSmall = ({
+  text,
+  backgroundColor,
+  textColor,
+  onClick,
+  border,
+}: ButtonTypes) => {
+  return (
+    <SmallButton
+      backgroundColor={backgroundColor}
+      textColor={textColor}
+      onClick={onClick}
+      border={border}
+    >
+      <p>{text}</p>
+    </SmallButton>
+  );
+};
+
+const LargeButtonSingle = styled.div<ButtonStyleTypes>`
+  width: 100%;
+  border-radius: 8px;
+  background: ${(props) => props.backgroundColor};
+  border: ${(props) => (props.border ? props.border : "none")};
+
+  cursor: pointer;
+
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  user-drag: none;
+  -ms-user-select: none;
+  -moz-user-select: -moz-none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+  user-select: none;
+
+  p {
+    padding: 14px 0;
+
+    color: ${(props) => props.textColor};
+    text-align: center;
+    font-family: "Pretendard";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+`;
 
 const LargeButton = styled.div<ButtonStyleTypes>`
   width: 100%;
   border-radius: 8px;
   background: ${(props) => props.backgroundColor};
+  border: ${(props) => (props.border ? props.border : "none")};
 
   cursor: pointer;
 
@@ -72,37 +164,3 @@ const SmallButton = styled.div<ButtonStyleTypes>`
     line-height: normal;
   }
 `;
-
-export const ButtonLarge = ({
-  text,
-  backgroundColor,
-  textColor,
-  onClick,
-}: ButtonTypes) => {
-  return (
-    <LargeButton
-      backgroundColor={backgroundColor}
-      textColor={textColor}
-      onClick={onClick}
-    >
-      <p>{text}</p>
-    </LargeButton>
-  );
-};
-
-export const ButtonSmall = ({
-  text,
-  backgroundColor,
-  textColor,
-  onClick,
-}: ButtonTypes) => {
-  return (
-    <SmallButton
-      backgroundColor={backgroundColor}
-      textColor={textColor}
-      onClick={onClick}
-    >
-      <p>{text}</p>
-    </SmallButton>
-  );
-};
