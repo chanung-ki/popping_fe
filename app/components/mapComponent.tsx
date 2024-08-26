eaff4e938391f1676611e744a84873fe86785e6a
HarenKei <seungmin1266@gmail.com>
2024-08-27 00:26:31 +0900

    Feat: Add List of Store card for map

--- app/components/mapComponent.tsx
+++ app/components/mapComponent.tsx
@@ -4,8 +4,10 @@ import styled from "styled-components";
 import { IconUser } from "./icons";
 import { Loading } from "./loading";
 import { fadeIn } from "@/public/utils/keyframe";
-import { IconChevronLeft, IconSearch, IconX } from "./icons";
+import { IconChevronLeft, IconSearch, IconX, IconMarker } from "./icons";
 import StyledSelect from "./styledSelect";
+import StoreInformation from "./storeInformations/StoreInformation";
+import StoreCard from "./popup-map/StoreCard";
 
 declare global {
   interface Window {
@@ -50,7 +52,9 @@ const MapComponent: React.FC = () => {
   const [isButtonVisible, setIsButtonVisible] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  //이외 states
+  // 팝업 스토어 목록
+  const [storeList, setStoreList] = useState<any[]>([]);
+  // 검색 영역 활성화
   const [isActiveSearch, setIsActiveSearch] = useState<boolean>(false);
   // 선택된 지역
   const [selectedLocation, setSelectedLocation] = useState<string>("");
@@ -135,8 +139,18 @@ const MapComponent: React.FC = () => {
           ),
           map: mapRef.current,
           icon: {
-            content: `<div style="background-color: ${COLORS.mainColor}; padding: 5px; border-radius: 50%; width: 10px; height: 10px; display: flex; justify-content: center; align-items: center;">
-                    </div>`,
+            content: `<svg xmlns="http://www.w3.org/2000/svg" width="28" height="41" viewBox="0 0 28 41" fill="none">
+  <g clip-path="url(#clip0_821_46)">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M14.0037 0C6.27056 0 0 6.32055 0 14.1154C0 23.188 14.0037 40.3276 14.0037 40.3276C14.0037 40.3276 28.0075 23.188 28.0075 14.1154C28 6.32055 21.7369 0 14.0037 0ZM14.0037 19.1507C11.2435 19.1507 9.0008 16.8977 9.0008 14.1078C9.0008 11.318 11.236 9.06501 14.0037 9.06501C16.7715 9.06501 19.0067 11.318 19.0067 14.1078C19.0067 16.8977 16.7715 19.1507 14.0037 19.1507Z" fill="#FA8D0E"/>
+    <path d="M19.9574 10.7912C19.5334 12.1558 19.7684 13.6397 20.5933 14.8065C21.6146 16.2506 20.5966 18.2479 18.8281 18.2709C17.3992 18.2895 16.0605 18.9715 15.206 20.1167C14.148 21.5341 11.9339 21.1837 11.3659 19.5086C10.9071 18.1555 9.84463 17.093 8.49149 16.6341C6.81648 16.0657 6.46567 13.852 7.88341 12.794C9.02829 11.9394 9.71061 10.6003 9.72917 9.1718C9.75182 7.40321 11.7491 6.38564 13.1935 7.40655C14.3603 8.23145 15.8441 8.46644 17.2088 8.04249C18.8979 7.51755 20.483 9.10275 19.9581 10.7919L19.9574 10.7912Z" fill="white"/>
+    <path d="M17.0739 9.54188L15.5266 9.95655C14.7946 10.1526 14.5496 11.0677 15.0852 11.6038L16.2179 12.7364C16.7536 13.2721 17.6687 13.0271 17.865 12.295L18.2797 10.7477C18.4757 10.0156 17.806 9.34586 17.0739 9.54188Z" fill="#FA8D0E"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_821_46">
+      <rect width="28" height="40.32" fill="white"/>
+    </clipPath>
+  </defs>
+</svg>`,
             anchor: new window.naver.maps.Point(5, 5), // 마커의 중심점을 설정
           },
         });
@@ -273,6 +287,24 @@ const MapComponent: React.FC = () => {
             setIsOpenMenu(!isOpenMenu);
           }}
         />
+
+        {isOpenMenu && (
+          <StoreInformationList>
+            {/*이 부분에 리스트 렌더링 (map) */}
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+            <StoreCard />
+          </StoreInformationList>
+        )}
       </SlideBottomMenu>
     </Container>
   );
@@ -430,6 +462,7 @@ const SlideBottomMenu = styled.div<{ $isOpen: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 
   width: 100%;
   height: ${(props) => (props.$isOpen ? "568px" : "100px")};
@@ -457,6 +490,18 @@ const ToggleButton = styled.button`
   border-radius: 2px;
   background-color: ${COLORS.greyColor};
   cursor: pointer;
+`;
+
+const StoreInformationList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: row wrap;
+  gap: 28px;
+  margin: 40px 0px;
+  width: 100%;
+  overflow-y: auto;
+  width: 100%;
 `;
 
 export default MapComponent;
---
+++ app/components/popup-map/StoreCard.tsx
@@ -0,0 +1,72 @@
+"use client";
+import styled from "styled-components";
+import { COLORS } from "@/public/styles/colors";
+import { IconHeart } from "../icons";
+import { useState } from "react";
+
+const StoreCard: React.FC = () => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  return (
+    <StoreCardContainer>
+      {/*StoreThumbnail에 이미지가 들어가도록 변경 필요*/}
+      <StoreThumbnail>
+        <div className={"icon"} onClick={() => setIsLiked(!isLiked)}>
+          <IconHeart
+            color={isLiked ? COLORS.mainColor : COLORS.lightGreyColor}
+            width={16}
+            height={15}
+          />
+        </div>
+      </StoreThumbnail>
+      <p className={"store-name"}>일릭서 스토어</p>
+      <p className={"store-desc"}>
+        일어나라 노예들이여 이 텍스트는 두줄까지만 가능
+      </p>
+    </StoreCardContainer>
+  );
+};
+
+const StoreCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  width: 166px;
+
+  .store-name {
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+
+  .store-desc {
+    width: 100%;
+    font-family: Pretendard;
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+  }
+`;
+
+const StoreThumbnail = styled.div`
+  position: relative;
+  width: 166px;
+  height: 166px;
+
+  border-radius: 8px;
+  background-color: ${COLORS.greyColor};
+
+  .icon {
+    position: absolute;
+    z-index: 1;
+    top: 138px;
+    left: 138px;
+
+    cursor: pointer;
+  }
+`;
+
+export default StoreCard;
