5213c6c27d77974b367220be15cfad6f39a6b4c9
HarenKei <seungmin1266@gmail.com>
2024-08-28 02:04:52 +0900

    Feat: Seperate Component of StoreDescription

--- app/components/mapComponent.tsx
+++ app/components/mapComponent.tsx
@@ -1,7 +1,7 @@
 import { COLORS } from "@/public/styles/colors";
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import { IconHeart, IconUser } from "./icons";
+import { IconUser } from "./icons";
 import { Loading } from "./loading";
 import { IconChevronLeft, IconSearch, IconX } from "./icons";
 import StyledSelect from "./styledSelect";
@@ -9,6 +9,7 @@ import StoreCard from "./popup-map/StoreCard";
 import { Store } from "@/public/utils/types";
 import Image from "next/image";
 import { formatDate } from "@/public/utils/function";
+import StoreDescription from "./popup-map/StoreDescription";
 
 declare global {
   interface Window {
@@ -361,53 +362,7 @@ const MapComponent: React.FC = () => {
             ))}
           </StoreInformationList>
         ) : (
-          <StoreDescContainer>
-            <StoreDescThumbnail>
-              <GradientOverlay />
-              <Image src={"/images/popping-banner.png"} fill alt={"썸네일"} />
-              <div className={"back-to-list"}>
-                <IconChevronLeft
-                  width={9}
-                  height={16}
-                  color={COLORS.whiteColor}
-                />
-              </div>
-            </StoreDescThumbnail>
-
-            <DescContainer>
-              <div className={"store-title-container"}>
-                <div className={"store-title"}>일릭서 스토어</div>
-                <div className={"store-like"}>
-                  <div
-                    className={"icon"}
-                    onClick={() => {
-                      setIsLikedStore(!isLikedStore);
-                    }}
-                  >
-                    <IconHeart
-                      width={32}
-                      height={30}
-                      color={isLikedStore ? COLORS.mainColor : COLORS.greyColor}
-                    />
-                  </div>
-                  <div className={"like-count"}>99만</div>
-                </div>
-              </div>
-              <div className={"store-desc"}>
-                일어나라 노예들이여 이 텍스트는 무한정 늘릴 수 있긴 하다네요
-                김태은 편도선 수술 무사 회복을 기원하는 마음으로 컴포넌트를
-                만들고 있습니다.
-              </div>
-
-              <div className={"store-info"}>
-                <div className={"store-address"}>
-                  서울특별시 용산구 한강대로 어딘가
-                </div>
-                <div className={"store-date"}>2024.07.24 ~ 2024.08.15</div>
-              </div>
-            </DescContainer>
-            <VisitButton>방문하기</VisitButton>
-          </StoreDescContainer>
+          <StoreDescription />
         )}
       </SlideBottomMenu>
     </Container>
@@ -603,145 +558,6 @@ const StoreInformationList = styled.div`
   width: 100%;
   overflow-y: auto;
   width: 100%;
-`;
-
-const StoreDescContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 16px;
-
-  margin: 40px 0px;
-
-  width: 100%;
-`;
-
-const StoreDescThumbnail = styled.div`
-  position: relative;
-  width: 100%;
-  height: 215px;
-  overflow: hidden;
-
-  /* background-color: ${COLORS.greyColor}; */
-
-  & > img {
-    position: relative;
-    object-fit: cover;
-  }
-
-  .back-to-list {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    position: absolute;
-    z-index: 5;
-    top: 20px;
-    left: 20px;
-  }
-`;
-
-const GradientOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 50px;
-  background: linear-gradient(to bottom, rgba(103, 102, 102, 0.5), transparent);
-  z-index: 4; /* 그라데이션이 이미지 위에 오도록 z-index 설정 */
-`;
-
-const DescContainer = styled.div`
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-
-  padding: 0px 20px;
-  width: calc(100% - 40px);
-  overflow-y: auto;
-
-  .store-title-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    width: 100%;
-
-    .store-like {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 4px;
-    }
-
-    .like-count {
-      text-align: center;
-      font-family: Pretendard;
-      font-size: 12px;
-      font-style: normal;
-      font-weight: 500;
-      line-height: normal;
-    }
-  }
-
-  .store-title {
-    font-family: Pretendard;
-    font-size: 32px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-  }
-
-  .store-desc {
-    font-family: Pretendard;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 300;
-    line-height: normal;
-
-    padding-right: 80px;
-    margin-top: 8px;
-  }
-
-  .store-info {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-
-    margin-top: 32px;
-    padding-right: 80px;
-
-    font-family: Pretendard;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-  }
-`;
-
-const VisitButton = styled.button`
-  all: unset;
-
-  position: absolute;
-  bottom: 16px;
-  z-index: 4;
-
-  width: calc(100% - 40px);
-  height: 48px;
-
-  border-radius: 8px;
-  background-color: ${COLORS.mainColor};
-  color: ${COLORS.whiteColor};
-
-  text-align: center;
-  font-family: Pretendard;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-
-  cursor: pointer;
 `;
 
 export default MapComponent;
